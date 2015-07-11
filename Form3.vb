7f41648e6b7b30843934cca30a63da489fff0420
xdavidwu <david20000612@gmail.com>
2015-07-11 19:14:58 +0800

    Form3: make code shorter and remove unused stuff

--- Form3.vb
+++ Form3.vb
@@ -1,6 +1,6 @@
 ﻿Imports XADB.ClassXADB
 Public Class Form3
-    Dim SelectedFloder As String, shellex As String, pro As New Process, GoToPath As String, GoToPathBak As String, ParentPath As String
+    Dim SelectedFloder As String, GoToPath As String, GoToPathBak As String
     Dim ChkTypePC As String = My.Application.Info.DirectoryPath + "\XADBChkType.sh", copyfrom As String, copyto As String
     Dim createdir As String, execpart As String, execwobb As String, chktypeandroid As String
     Dim useRoot As Boolean, useBusybox As Boolean, changeToSd As Boolean
@@ -8,24 +8,7 @@ Public Class Form3
     Dim deviceRunning As String
 
     Private Sub ListBox1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListBox1.DoubleClick
-        shellex = execpart
-        SelectedFloder = ListBox1.SelectedItem
-        GoToPathBak = GoToPath
-        GoToPath = Replace(GoToPath + SelectedFloder, vbCr, "")
-        shellex = shellex + " sh" + chktypeandroid + GoToPath
-        Dim sOutput As String = execInShellReturnOutput(shellex)
-        If sOutput.StartsWith("file") Then
-            Call DeviceFileBrowser_PullFile()
-        ElseIf sOutput.StartsWith("folder") Then
-            GoToPath = GoToPath + "/"
-            Call DeviceFileBrowser_EnterFloder()
-        Else
-            If GoToPath.Contains(" ") Then
-                MsgBox("Couldn't load " + GoToPath + vbCrLf + "Spaces are not allowed in the name of file / folder.")
-            Else
-                MsgBox("Couldn't load " + GoToPath + vbCrLf + sOutput)
-            End If
-        End If
+        DeviceFileBrowser_CheckTypeAndEnter()
     End Sub
 
     Private Sub Form3_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
@@ -37,33 +20,24 @@ Public Class Form3
         End If
         execpart = Form1.ADBPath + " -s " + deviceRunning + " shell "
         If useRoot = True Then
-            execpart = execpart + " su -c "
+            execpart += " su -c "
             ActionToolStripMenuItem.Visible = True
-            shellex = Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.build.type"
-            Dim sOutput2 As String = execInShellReturnOutput(shellex)
-            ToolStripStatusLabel1.Text = "Running with root"
+            Dim sOutput2 As String = execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.build.type")
+            ToolStripStatusLabel1.Text = "Running as root"
             If sOutput2.StartsWith("userdebug") Then
                 Call ADBRootAndRemount()
-                ToolStripStatusLabel1.Text = ToolStripStatusLabel1.Text + " on userdebug build"
+                ToolStripStatusLabel1.Text += " on userdebug build"
             ElseIf sOutput2.StartsWith("eng") Then
                 Call ADBRootAndRemount()
-                ToolStripStatusLabel1.Text = ToolStripStatusLabel1.Text + " on eng build"
+                ToolStripStatusLabel1.Text += " on eng build"
             End If
         Else
             ToolStripStatusLabel1.Text = "Running without root premission"
         End If
         execwobb = execpart
-        If useBusybox = True Then execpart = execpart + " busybox "
+        If useBusybox = True Then execpart += " busybox "
         GoToPath = "/"
-        shellex = execwobb
-        shellex = shellex + " ls"
-        Dim sOutput As String = execInShellReturnOutput(shellex)
-        Dim soutputspilt() As String = Split(sOutput, vbCrLf)
-        Dim a As Byte
-        For a = 0 To soutputspilt.GetUpperBound(0)
-            ListBox1.Items.Add(soutputspilt(a))
-        Next
-        ListBox1.SelectedIndex = 0
+        DeviceFileBrowser_EnterFloder()
         Call PushChkTypeScript()
     End Sub
 
@@ -75,13 +49,27 @@ Public Class Form3
             Call DeviceFileBrowser_EnterFloder()
         End If
     End Sub
-
-
+    
+    Private Sub DeviceFileBrowser_CheckTypeAndEnter()
+        SelectedFloder = ListBox1.SelectedItem
+        GoToPathBak = GoToPath
+        GoToPath = Replace(GoToPath + SelectedFloder, vbCr, "")
+        Dim sOutput As String = execInShellReturnOutput(execpart + " sh" + chktypeandroid + GoToPath)
+        If sOutput.StartsWith("file") Then
+            Call DeviceFileBrowser_PullFile()
+        ElseIf sOutput.StartsWith("folder") Then
+            GoToPath += "/"
+            Call DeviceFileBrowser_EnterFloder()
+        ElseIf GoToPath.Contains(" ") Then
+            MsgBox("Couldn't load " + GoToPath + vbCrLf + "Spaces are not allowed in the name of file / folder.")
+        Else
+            MsgBox("Couldn't load " + GoToPath + vbCrLf + sOutput)
+        End If
+    End Sub
+    
     Private Sub DeviceFileBrowser_EnterFloder()
         TextBox1.Text = GoToPath
-        shellex = execwobb + " ls " + GoToPath
-        Dim sOutput As String = execInShellReturnOutput(shellex)
-        Dim soutputspilt() As String = Split(sOutput, vbCrLf)
+        Dim soutputspilt() As String = Split(execInShellReturnOutput(execwobb + " ls " + GoToPath), vbCrLf)
         Dim a As Byte
         ListBox1.Items.Clear()
         For a = 0 To soutputspilt.GetUpperBound(0)
@@ -89,33 +77,15 @@ Public Class Form3
         Next
         ListBox1.SelectedIndex = 0
     End Sub
+    
     Private Sub DeviceFileBrowser_PullFile()
-        shellex = Form1.ADBPath + "-s " + deviceRunning + " pull " + GoToPath
-        execInShellReturnOutput(shellex)
-        shellex = "start " + My.Application.Info.DirectoryPath + "\" + SelectedFloder
-        execInShellReturnOutput(shellex)
+        execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " pull " + GoToPath)
+        execInShellReturnOutput("start " + My.Application.Info.DirectoryPath + "\" + SelectedFloder)
     End Sub
 
     Private Sub ListBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles ListBox1.KeyPress
         If e.KeyChar = Chr(13) Then
-            shellex = execpart
-            SelectedFloder = ListBox1.SelectedItem
-            GoToPathBak = GoToPath
-            GoToPath = Replace(GoToPath + SelectedFloder, vbCr, "")
-            shellex = shellex + " sh" + chktypeandroid + GoToPath
-            Dim sOutput As String = execInShellReturnOutput(shellex)
-            If sOutput.StartsWith("file") Then
-                Call DeviceFileBrowser_PullFile()
-            ElseIf sOutput.StartsWith("folder") Then
-                GoToPath = GoToPath + "/"
-                Call DeviceFileBrowser_EnterFloder()
-            Else
-                If GoToPath.Contains(" ") Then
-                    MsgBox("Couldn't load " + GoToPath + vbCrLf + "Spaces are not allowed in the name of file / folder.")
-                Else
-                    MsgBox("Couldn't load " + GoToPath + vbCrLf + sOutput)
-                End If
-            End If
+            DeviceFileBrowser_CheckTypeAndEnter()
         End If
     End Sub
 
@@ -123,32 +93,22 @@ Public Class Form3
         If fileext.FileExists(ChkTypePC) = False Then
             MsgBox("Type checking script was not found!" + vbCrLf + "Please put XADBChkType.sh in " + vbCrLf + My.Application.Info.DirectoryPath)
         End If
-        shellex = "/c " + Form1.ADBPath + "-s " + deviceRunning + " push " + ChkTypePC + chktypeandroid
-        execInShellReturnOutput(shellex)
+        execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " push " + ChkTypePC + chktypeandroid)
     End Sub
 
     Public Sub DeleteFileOrFolder()
-        shellex = execpart
         SelectedFloder = ListBox1.SelectedItem
         GoToPathBak = GoToPath
         GoToPath = Replace(GoToPath + SelectedFloder, vbCr, "")
-        shellex = shellex + " sh" + chktypeandroid + GoToPath
-        Dim sOutput As String = execInShellReturnOutput(shellex)
+        Dim sOutput As String = execInShellReturnOutput(execpart + " sh" + chktypeandroid + GoToPath)
         If sOutput.StartsWith("file") Then
-            shellex = execpart
-            shellex = shellex + " rm " + GoToPath
-            execInShellReturnOutput(shellex)
-            GoToPath = GoToPathBak
+            execInShellReturnOutput(execpart + " rm " + GoToPath)
         ElseIf sOutput.StartsWith("folder") Then
-            shellex = execpart
-            shellex = shellex + " rmdir -r " + GoToPath
-            Dim prosi2 As New ProcessStartInfo("cmd.exe", shellex)
-            execInShellReturnOutput(shellex)
-            GoToPath = GoToPathBak
+            execInShellReturnOutput(execpart + " rmdir -r " + GoToPath)
         Else
             MsgBox(sOutput)
-            GoToPath = GoToPathBak
         End If
+        GoToPath = GoToPathBak
     End Sub
 
     Private Sub DeleteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DeleteToolStripMenuItem.Click
@@ -156,13 +116,11 @@ Public Class Form3
             Call DeleteFileOrFolder()
             Call DeviceFileBrowser_EnterFloder()
         End If
-
     End Sub
+    
     Public Sub ADBRootAndRemount()
-        shellex = "/c " + Form1.ADBPath + "-s " + deviceRunning + " root"
-        execInShellReturnOutput(shellex)
-        shellex = "/c " + Form1.ADBPath + "-s " + deviceRunning + " remount"
-        execInShellReturnOutput(shellex)
+        execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " root")
+        execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " remount")
     End Sub
 
     Private Sub PullToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PullToolStripMenuItem.Click
@@ -185,12 +143,10 @@ Public Class Form3
     End Sub
 
     Private Sub PasteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PasteToolStripMenuItem.Click
-        shellex = execpart
         SelectedFloder = ListBox1.SelectedItem
         GoToPathBak = GoToPath
         GoToPath = Replace(GoToPath + SelectedFloder, vbCr, "")
-        shellex = shellex + " sh" + chktypeandroid + GoToPath
-        Dim sOutput As String = execInShellReturnOutput(shellex)
+        Dim sOutput As String = execInShellReturnOutput(execpart + " sh" + chktypeandroid + GoToPath)
         If sOutput.StartsWith("file") Then
             GoToPath = GoToPathBak
             copyto = GoToPath
@@ -202,9 +158,7 @@ Public Class Form3
             GoToPath = GoToPathBak
         End If
         If MsgBox("Copy " + copyfrom + " to " + copyto + "?", MsgBoxStyle.OkCancel) = MsgBoxResult.Ok Then
-            shellex = execpart
-            shellex = shellex + "cp " + copyfrom + " " + copyto
-            execInShellReturnOutput(shellex)
+            execInShellReturnOutput(execpart + "cp " + copyfrom + " " + copyto)
             Call DeviceFileBrowser_EnterFloder()
         End If
     End Sub
@@ -220,9 +174,7 @@ Public Class Form3
                 MsgBox("No spaces are allowed in name!")
             Else
                 If MsgBox("Rename " + renamefrom + " to " + renameto + "?", MsgBoxStyle.OkCancel) = MsgBoxResult.Ok Then
-                    shellex = execpart
-                    shellex = shellex + " mv " + renamefrom + " " + renameto
-                    execInShellReturnOutput(shellex)
+                    execInShellReturnOutput(execpart + " mv " + renamefrom + " " + renameto)
                     Call DeviceFileBrowser_EnterFloder()
                 End If
             End If
@@ -235,23 +187,20 @@ Public Class Form3
             MsgBox("No spaces are allowed in name!")
         Else
             If MsgBox("Create " + createdir + "?", MsgBoxStyle.OkCancel) = MsgBoxResult.Ok Then
-                shellex = execpart
-                shellex = shellex + " mkdir " + createdir
-                execInShellReturnOutput(shellex)
+                execInShellReturnOutput(execpart + " mkdir " + createdir)
                 Call DeviceFileBrowser_EnterFloder()
             End If
         End If
     End Sub
 
     Private Sub SystemAsRwToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SystemAsRwToolStripMenuItem.Click
-        shellex = "/c " + Form1.ADBPath + "-s " + deviceRunning + " shell su -c mount -ro remount,rw /system"
-        execInShellReturnOutput(shellex)
+        execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell su -c mount -ro remount,rw /system")
     End Sub
 
     Private Sub AsRwToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AsRwToolStripMenuItem.Click
-        shellex = "/c " + Form1.ADBPath + "-s " + deviceRunning + " shell su -c mount -ro remount,rw /"
-        execInShellReturnOutput(shellex)
+        execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell su -c mount -ro remount,rw /")
     End Sub
+    
     Public Sub New(ByVal root As Boolean, ByVal bbox As Boolean, ByVal chtosd As Boolean, ByVal device As String)
         InitializeComponent()
         useRoot = root
