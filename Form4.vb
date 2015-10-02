ace6352faad49d6d63b0c884da15750f8d8cc2e2
xdavidwu <david20000612@gmail.com>
2015-10-02 21:49:40 +0800

    Fix Form4

--- Form4.vb
+++ Form4.vb
@@ -7,24 +7,24 @@ Public Class Form4
     End Sub
     Private Sub Form4_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.Text = "XADB-Device Info-" + deviceRunning
-        Label2.Text = execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.product.manufacturer") _
-        + execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.product.device") _
-        + execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.build.version.release") _
-        + execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.build.type") _
-        + execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.build.date") _
-        + execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.build.description")
+        Label2.Text = execInShellReturnOutput(Form1.ADBPath + "s " + deviceRunning + " shell getprop ro.product.manufacturer") _
+        + execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.product.device") _
+        + execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.build.version.release") _
+        + execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.build.type") _
+        + execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.build.date") _
+        + execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.build.description")
         Dim cmVer As String = execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.cm.version")
         If Replace(cmVer, vbCrLf, "").TrimEnd <> "" Then
             Label1.Text = Label1.Text + vbCrLf + vbCrLf + "Cyanogenmod version:"
             Label2.Text = Label2.Text + cmVer
             Label1.Text = Label1.Text + vbCrLf + vbCrLf + "Cyanogenmod codename:"
-            Label2.Text = Label2.Text + execInShellReturnOutput("adb -s " + deviceRunning + " shell getprop ro.cm.device")
+            Label2.Text = Label2.Text + execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell getprop ro.cm.device")
         End If
-        RichTextBox1.Text = Replace(execInShellReturnOutput("adb -s " + deviceRunning + " shell cat /proc/cpuinfo"), vbCr, "")
+        RichTextBox1.Text = Replace(execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell cat /proc/cpuinfo"), vbCr, "")
         refreshMemInfo()
     End Sub
     Public Sub refreshMemInfo()
-        RichTextBox2.Text = Replace(execInShellReturnOutput("adb -s " + deviceRunning + " shell cat /proc/meminfo"), vbCr, "")
+        RichTextBox2.Text = Replace(execInShellReturnOutput(Form1.ADBPath + "-s " + deviceRunning + " shell cat /proc/meminfo"), vbCr, "")
     End Sub
 
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
